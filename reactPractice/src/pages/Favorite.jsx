091d672a7bda224d9e2dd5ef871f70a1f6311e95
Allon Nam <allon.nam96@gmail.com>
2025-07-30 19:06:27 -0400

    time to do css

---
+++ reactPractice/src/pages/Favorite.jsx
@@ -0,0 +1,8 @@
+function Favorite() {
+    return <div className="favorites-empty">
+        <h2>No favorite movies yet</h2>
+        <p>Start adding your favorite movies</p>
+    </div>
+}
+
+export default Favorite
