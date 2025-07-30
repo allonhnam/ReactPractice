15a52939ff8d4afc97182fec076b1d51ac3fc9bd
Allon Nam <allon.nam96@gmail.com>
2025-07-30 18:55:28 -0400

    home

--- reactPractice/src/pages/Home.jsx
+++ reactPractice/src/pages/Home.jsx
@@ -2,13 +2,18 @@ import MovieCard from "../components/MovieCard";
 import { useState } from "react";
 
 function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
+
   const movies = [
     { id: 1, title: "John Wick", release_date: "2020" },
     { id: 2, title: "terminator", release_date: "1999" },
     { id: 3, title: "matrix", release_date: "2021" },
   ];
 
-  const handleSearch = () => {};
+  const handleSearch = (e) => {
+    e.preventDefault();
+    alert(searchQuery);
+  };
 
   return (
     <div className="home">
@@ -17,15 +22,20 @@ function Home() {
           type="text"
           placeholder="Search for movies..."
           className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
         />
         <button type="submit" className="search button">
           Search
         </button>
       </form>
       <div className="movies-grid">
-        {movies.map((movie) => (
-          <MovieCard movie={movie} key={movie.id} />
-        ))}
+        {movies.map(
+          (movie) =>
+            (
+              <MovieCard movie={movie} key={movie.id} />
+            )
+        )}
       </div>
     </div>
   );
